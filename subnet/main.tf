d0a055904e382b09a8de933c97ad78be25812ace
Andrey Dovbnya <andrey.dovbnya@namecheap.com>
2019-11-21 16:41:27 +0200

    add security group

--- subnet/main.tf
+++ subnet/main.tf
@@ -1,19 +1,12 @@
 resource "aws_subnet" "this" {
   cidr_block = var.cidr_block
   vpc_id = var.vpc_id
+  availability_zone = var.availability_zone_name
+  map_public_ip_on_launch = true
   tags = var.tags
-}
-
-resource "aws_route_table" "this" {
-  vpc_id = var.vpc_id
-  tags = var.tags
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = var.ig_id
-  }
 }
 
 resource "aws_route_table_association" "this" {
-  route_table_id = aws_route_table.this.id
+  route_table_id = var.aws_route_table_id
   subnet_id = aws_subnet.this.id
 }
--- subnet/variables.tf
+++ subnet/variables.tf
@@ -10,6 +10,14 @@ variable "ig_id" {
   type = string
 }
 
+variable "availability_zone_name" {
+  type = string
+}
+
+variable "aws_route_table_id" {
+  type = string
+}
+
 variable "tags" {
   type = map(string)
   default = { Name = "dev1", Description = "Managed by terraform" }
